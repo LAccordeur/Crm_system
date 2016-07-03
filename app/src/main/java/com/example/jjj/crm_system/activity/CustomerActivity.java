3667017a0cd7b87f0a5b165c8d1471463d8a325e
lpty <1079994170@qq.com>
2016-07-03 17:26:19 +0800

    2016/7/3
    login&merchantactivity

---
+++ app/src/main/java/com/example/jjj/crm_system/activity/CustomerActivity.java
@@ -0,0 +1,34 @@
+package com.example.jjj.crm_system.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.jjj.crm_system.R;
+import com.example.jjj.crm_system.ui.Base.BaseActivity;
+
+public class CustomerActivity extends BaseActivity {
+
+    @Override
+    protected int getRootView() {
+        return R.layout.activity_customer;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+}
---
+++ app/src/main/java/com/example/jjj/crm_system/activity/CustomerloginActivity.java
@@ -0,0 +1,64 @@
+package com.example.jjj.crm_system.activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.jjj.crm_system.R;
+import com.example.jjj.crm_system.ui.Base.BaseActivity;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class CustomerloginActivity extends BaseActivity {
+    private EditText et_phonenum_customerlogin;
+    private Button bt_sendVerificationcode_customerlogin;
+    private EditText et_VerificationCode_customerlogin;
+    private TextView tv_back_customerlogin;
+
+
+    @Override
+    protected int getRootView() {
+        return R.layout.activity_customerlogin;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void initData() {
+        et_phonenum_customerlogin = (EditText)findViewById(R.id.et_phonenum_customerlogin);
+        et_VerificationCode_customerlogin = (EditText)findViewById(R.id.et_VerificationCode_customerlogin);
+        bt_sendVerificationcode_customerlogin = (Button)findViewById(R.id.bt_sendVerificationcode_customerlogin);
+        tv_back_customerlogin = (TextView)findViewById(R.id.tv_back_customerlogin);
+
+    }
+
+    @Override
+    protected void setListener() {
+        tv_back_customerlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        bt_sendVerificationcode_customerlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CustomerloginActivity.this,CustomerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
