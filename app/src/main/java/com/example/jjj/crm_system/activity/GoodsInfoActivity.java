6fadfa8ed1cbe1ca831a6cdadf00e09825d13724
jkllpty <1079994170@qq.com>
2016-09-03 15:44:12 +0800

    goodsinfo
    用户端的一些小修改

--- app/src/main/java/com/example/jjj/crm_system/activity/GoodsInfoActivity.java
+++ app/src/main/java/com/example/jjj/crm_system/activity/GoodsInfoActivity.java
@@ -1,9 +1,12 @@
 package com.example.jjj.crm_system.activity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.example.jjj.crm_system.R;
 import com.example.jjj.crm_system.ui.Base.BaseActivity;
@@ -11,12 +14,15 @@ import com.example.jjj.crm_system.utils.ActivityUtil;
 
 public class GoodsInfoActivity extends BaseActivity {
     private ImageView iv_back;
+    private String top_background;
+    private RelativeLayout rl_top;
 
     /**
      * 加载UI前的预初始化
      */
     @Override
     protected void init() {
+        //top_background = (String)getIntent().getStringExtra("top_background");
 
     }
 
@@ -50,6 +56,11 @@ public class GoodsInfoActivity extends BaseActivity {
     @Override
     protected void initData() {
         iv_back = (ImageView)findViewById(R.id.iv_back_goodsinfo);
+
+       // if(top_background!=null&&top_background!=""){
+           // rl_top.setBackgroundColor(Color.parseColor(top_background));
+         //   rl_top.setBackgroundColor(Color.RED);
+       // }
 
     }
 }
--- app/src/main/java/com/example/jjj/crm_system/fragment/HomeFragment.java
+++ app/src/main/java/com/example/jjj/crm_system/fragment/HomeFragment.java
@@ -1,8 +1,13 @@
 package com.example.jjj.crm_system.fragment;
 
+import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.jjj.crm_system.R;
+import com.example.jjj.crm_system.activity.GoodsInfoActivity;
+import com.example.jjj.crm_system.activity.MerchintInfoActivity;
 import com.example.jjj.crm_system.ui.Base.BaseFragment;
 
 /**
@@ -13,6 +18,8 @@ import com.example.jjj.crm_system.ui.Base.BaseFragment;
  * 商家首页
  */
 public class HomeFragment extends BaseFragment{
+    private TextView tv_details,tv_acconthome;
+    private Button bt_goods;
     @Override
     protected int getRootView() {
         return R.layout.fragment_home;
@@ -25,11 +32,31 @@ public class HomeFragment extends BaseFragment{
 
     @Override
     protected void initData(View view) {
+        tv_acconthome = (TextView)view.findViewById(R.id.tv_acconthome);
+        tv_details = (TextView)view.findViewById(R.id.tv_details_home);
+        bt_goods = (Button)view.findViewById(R.id.goods_bt);
 
     }
 
     @Override
     protected void setListener() {
+
+        bt_goods.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), GoodsInfoActivity.class);
+               // intent.putExtra("top_background","#fa9e26");
+                startActivity(intent);
+            }
+        });
+
+        tv_details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MerchintInfoActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
